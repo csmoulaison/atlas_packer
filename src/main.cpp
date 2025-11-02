8ef0c5ce5ba8985463f6890e38e4f49ab8b0c47f
Conner Moulaison <csmoulaison@gmail.com>
2025-11-01 17:12:44 -0700

    Scrap internet algo and implement shelf packing, outputting pixels to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <vector>
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
@@ -8,6 +7,7 @@
 #include "base/base.h"
 
 #define FONT_CHARS_LEN 128
+#define MAX_PACK_LEAVES 1024
 
 struct FontChar {
 	u8* pixels;
@@ -18,13 +18,133 @@ struct FontChar {
 
 struct PackRect {
 	u32 x, y, w, h;
-	bool packed = false;
+	i32 char_index;
 };
 
 // Represents an empty space in the atlas being packed.
 struct PackNode {
 	i32 x, y, w, h;
 };
+
+// { (u32) x, y, w, h,
+//   (i32) bearing,
+//   (u32) advance
+// } Chars[]
+struct SerialChar {
+	u32 x, y, w, h;
+	i32 bearing[2];
+	u32 advance;
+};
+
+bool pack(u32 font_size, i32 atlas_length, i32 atlas_area, 
+PackRect* rects, FontChar* chars) {
+	// Pack rects using shelf algorithm.
+	i32 curx = 0;
+	i32 cury = 0;
+	i32 cur_shelf_size = 0;
+	for(i32 i = 0; i < FONT_CHARS_LEN; i++) {
+		PackRect* rect = &rects[i];
+		if(cur_shelf_size == 0) {
+			cur_shelf_size = rect->h;
+		}
+
+		if(curx + rect->w > atlas_length) {
+			if(cur_shelf_size == 0) {
+				return false;
+			}
+
+			cury += cur_shelf_size;
+			cur_shelf_size = rect->h;
+			curx = 0;
+
+			printf("new shelf! x,y,h:%i,%i,%i\n", curx, cury, cur_shelf_size);
+		}
+
+		rect->x = curx;
+		rect->y = cury;
+		printf("Rect packed (x,y,w,h): %.3u,%.3u,%.3u,%.3u\n", 
+			rect->x,
+			rect->y,
+			rect->w,
+			rect->h);
+		curx += rect->w;
+
+		if(cury + cur_shelf_size >= atlas_length) {
+			return false;
+		}
+	}
+
+	// Render to atlas texture.
+	u8 atlas[atlas_area] = {};
+	for(i32 i = 0; i < FONT_CHARS_LEN; i++) {
+		PackRect* rect = &rects[i];
+		FontChar* ch = &chars[rect->char_index];
+		for(i32 y = 0; y < ch->size[1]; y++) {
+			for(i32 x = 0; x < ch->size[0]; x++) {
+				i32 dst_x = rect->x + x;
+				i32 dst_y = rect->y + y;
+				u8 pixel = ch->pixels[y * ch->size[0] + x];
+				atlas[dst_y * atlas_length + dst_x] = pixel;
+			}
+		}
+	}
+
+	SerialChar serial_chars[FONT_CHARS_LEN];
+	for(i32 i = 0; i < FONT_CHARS_LEN; i++) {
+		PackRect* rect = &rects[i];
+		FontChar* ch = &chars[rect->char_index];
+
+		SerialChar* schar = &serial_chars[rect->char_index];
+		schar->x = rect->x;
+		schar->y = rect->y;
+		schar->w = rect->w;
+		schar->h = rect->h;
+		schar->bearing[0] = ch->bearing[0];
+		schar->bearing[1] = ch->bearing[1];
+		schar->advance = ch->advance;
+	}
+
+	// Store in binary format.
+	// 
+	// FORMAT SPECIFICATION:
+	// (u32) AtlasLength
+	// (u32) NumChars
+	// { (u32) x, y, w, h,
+	//   (i32) bearing[2],
+	//   (u32) advance
+	// } Chars[]
+	// (u8) Pixels[]
+
+	Arena file_arena;
+	arena_init(&file_arena, GIGABYTE);
+
+	//*((u32*)arena_alloc(&file_arena, sizeof(u32))) = atlas_length;
+	//*((u32*)arena_alloc(&file_arena, sizeof(u32))) = FONT_CHARS_LEN;
+	//for(i32 i = 0; i < FONT_CHARS_LEN; i++) {
+	//	SerialChar* ch = &serial_chars[i];
+	//	
+	//	*((u32*)arena_alloc(&file_arena, sizeof(u32))) = ch->x;
+	//	*((u32*)arena_alloc(&file_arena, sizeof(u32))) = ch->y;
+	//	*((u32*)arena_alloc(&file_arena, sizeof(u32))) = ch->w;
+	//	*((u32*)arena_alloc(&file_arena, sizeof(u32))) = ch->h;
+
+	//	*((i32*)arena_alloc(&file_arena, sizeof(i32))) = ch->bearing[0];
+	//	*((i32*)arena_alloc(&file_arena, sizeof(i32))) = ch->bearing[1];
+	//	*((u32*)arena_alloc(&file_arena, sizeof(u32))) = ch->advance;;
+	//}
+	for(u32 i = 0; i < atlas_area; i++) {
+		*((u8*)arena_alloc(&file_arena, sizeof(u8))) = atlas[i];
+	}
+
+	// Write to file
+	FILE* f = fopen("out.cmfont", "w");
+	size_t s = fwrite(file_arena.region, 1, file_arena.index, f);
+	printf("Wrote %u bytes (%u)\n", s, file_arena.index);
+	printf("Image length: %u\n", atlas_length);
+	fclose(f);
+
+	return true;
+}
 
 int main(int argc, char** argv) {
 	// Validate arguments
@@ -57,15 +177,15 @@ int main(int argc, char** argv) {
 
 	FT_Set_Pixel_Sizes(face, 0, font_size);
 
-	Arena arena;
-	arena_init(&arena, GIGABYTE);
+	Arena pixel_arena;
+	arena_init(&pixel_arena, GIGABYTE);
 
-	FontChar char_list[FONT_CHARS_LEN];
+	FontChar chars[FONT_CHARS_LEN];
 
 	for(unsigned char c = 0; c < FONT_CHARS_LEN; c++) {
 		if(FT_Load_Char(face, c, FT_LOAD_RENDER)) { panic(); }
 
-		FontChar* ch = &char_list[c];
+		FontChar* ch = &chars[c];
 		ch->size[0] = face->glyph->bitmap.width;
 		ch->size[1] = face->glyph->bitmap.rows;
 		ch->bearing[0] = face->glyph->bitmap_left;
@@ -73,105 +193,43 @@ int main(int argc, char** argv) {
 		ch->advance = (u32)face->glyph->advance.x;
 
 		u32 tex_size = sizeof(u8) * ch->size[0] * ch->size[1];
-		ch->pixels = (u8*)arena_alloc(&arena, tex_size);
+		ch->pixels = (u8*)arena_alloc(&pixel_arena, tex_size);
 		memcpy(ch->pixels, face->glyph->bitmap.buffer, tex_size);
 	}
 
 	FT_Done_Face(face);
 	FT_Done_FreeType(ft);
 
-	/* Debug print characters.
-	for(u32 j = 0; j < FONT_CHARS_LEN; j++) {
-		FontChar* c = &char_list[j];
-		printf("CHARACTER\ns: %u %u, b: %u %u, a: %u\n", c->size[0], c->size[1], c->bearing[0], c->bearing[1], c->advance);
-		for(u32 i = 0; i < c->size[0] * c->size[1]; i++) {
-			if(i % c->size[0] == 0) { printf("\n"); }
-			float val = c->pixels[i];
-			char p = ' '; if(val > 0.5) { p = '#'; }
-			printf("%c", p);
-		}
-		getchar();
-	} */
+	// RECT PACKING:
+	PackRect rects[FONT_CHARS_LEN];
+	i32 rects_len = 0;
 
-
-	// NOW: CODE COPIED FROM:
-	// https://www.david-colson.com/2020/03/10/exploring-rect-packing.html
-	// 
-	// 1. Adapt to our style to understand. No vectors, snake case, etc.
-	// 2. Render to the packed rect locations.
-	// 3. Pack texture and character data into binary file and save to disk.
-	std::vector<PackRect> rects;
-	// NOW: Populate rects from FontChar data.
-	// - sort rects by area?
-	 
-	std::vector<PackNode> leaves;
-
-	// NOW: 700x700 is implicit here, I guess? We want to choose something relative
-	// to the chosen font size presumably, and we might want it to be a power of 2.
-	// 
-	// Our initial node is the entire image
-	leaves.push_back({0, 0, 700, 700});
-
-	for (PackRect& rect : rects)
-	{
-		bool done = false;
-		// Backward iterate over nodes
-		for (int i = (int)leaves.size() - 1; i >= 0 && !done; --i) {
-			PackNode& node = leaves[i];
-
-			// If the node is big enough, we've found a suitable spot for our rectangle
-			if (node.w > rect.w && node.h > rect.h) {
-				rect.x = node.x;
-				rect.y = node.y;
-
-				// Split the rectangle, calculating the unused space
-				int remainingWidth = node.w - rect.w;
-				int remainingHeight = node.h - rect.h;
-
-				PackNode newSmallerNode;
-				PackNode newLargerNode;
-				// We can work out which way we need to split by checking which
-				//  remaining dimension is larger
-				if (remainingHeight > remainingWidth) {
-					// The lesser split here will be the top right
-					newSmallerNode.x = node.x + rect.w;
-					newSmallerNode.y = node.y;
-					newSmallerNode.w = remainingWidth;
-					newSmallerNode.h = rect.h;
-
-					newLargerNode.x = node.x;
-					newLargerNode.y = node.y + rect.h;
-					newLargerNode.w = node.w;
-					newLargerNode.h = remainingHeight;
-				} else {
-					// The lesser split here will be the bottom left
-					newSmallerNode.x = node.x;
-					newSmallerNode.y = node.y + rect.h;
-					newSmallerNode.w = rect.w;
-					newSmallerNode.h = remainingHeight;
-
-					newLargerNode.x = node.x + rect.w;
-					newLargerNode.y = node.y;
-					newLargerNode.w = remainingWidth;
-					newLargerNode.h = node.h;
-				}
-
-				// Removing the node we're using up
-				leaves[i] = leaves.back();
-				leaves.pop_back();
-
-				// Push back the two new nodes, note smaller one last.
-				leaves.push_back(newLargerNode);
-				leaves.push_back(newSmallerNode);
-
-				done = true;
+	// Populate rects and sort by height. Just bubble sort, don't @ me.
+	for(u32 i = 0; i < FONT_CHARS_LEN; i++) {
+		PackRect* rect = &rects[i];
+		rect->x = 0;
+		rect->y = 0;
+		rect->w = chars[i].size[0];
+		rect->h = chars[i].size[1];
+		rect->char_index = i;
+	}
+	for(u32 i = 0; i < FONT_CHARS_LEN; i++) {
+		for(i32 j = 0; j < FONT_CHARS_LEN - 1; j++) {
+			if(rects[j].h < rects[j + 1].h) {
+				PackRect tmp = rects[j];
+				rects[j] = rects[j + 1];
+				rects[j + 1] = tmp;
 			}
 		}
+	}
 
-		if (!done) {
-			continue;
-		}
+	// TODO: Just keep trying to pack and double on each attempt.
+	i32 atlas_length = 2;
+	i32 atlas_area = 0;
+	while(pack(font_size, atlas_length, atlas_area, rects, chars) == false) {
+		atlas_length *= 2;
+		atlas_area = atlas_length * atlas_length;
 
-		rect.packed = true;
+		assert(atlas_length <= 4096);
 	}
 }
